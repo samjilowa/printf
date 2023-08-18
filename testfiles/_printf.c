f085b2fe47a03d969dbbea21b6aa8c472f8345f0
samjilowa <samueljilowa@gmail.com>
2023-08-18 06:15:04 -0700

    test file created

--- testfiles/_printf.c
+++ testfiles/_printf.c
@@ -3,38 +3,36 @@
 #include "main.h"
 #include "print_char.h"
 
-int _printf(const char *format, ...)
-  va_list args;
-int char_count = 0;
+int _printf(const char *format, ...) {
+    va_list args;
+    int char_count = 0; 
+    va_start(args, format);
 
-va_start(args, format);
-while (*format != '\0')
-  {
-    if (*format == '\%')
-      format++;
-      {
-	if (*format == '\0')
-	  {
-	    break;
-	  }
-	
-	switch (*format)
-	  {
-	  case 'c':
-	    char_count += print_char(args);
-	    break;
-	  default:
-	    putchar(*format);
-	    char_count++;
-	    break;
-	  }
-      }
-      else
-	putchar(*format);
-    char_count++;
-  }
-format++;
-va_end(args);
-}
-return char_count;
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            if (*format == '\0') {
+                break; /* Handle incomplete format */
+            }
+
+            switch (*format) {
+                case 'c':
+                    char_count += print_char(args);
+                    break;
+                default:
+                    putchar(*format);
+                    char_count++;
+                    break;
+            }
+        } else {
+            putchar(*format);
+            char_count++;
+        }
+        
+        format++;
+    }
+
+    va_end(args);
+
+    return char_count;
 }

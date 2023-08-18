6ef78266a53e6c8becf6a74401c2e7f0603a7c45
samjilowa <samueljilowa@gmail.com>
2023-08-18 06:26:27 -0700

    test file created

--- testfiles/print_char.c
+++ testfiles/print_char.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "print_char.h"
-int print_char(va_list args)
-{
-  char c = (char) va_args(args, int);
-  putchar (c);
-  return 1;
+
+int print_char(va_list args) {
+    char c = (char)va_arg(args, int);
+    putchar(c);
+    return 1;
 }
+

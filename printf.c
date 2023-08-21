24959b24947a5840b74846577b188410e213756a
KabzaKole <kabelomasela47@gmail.com>
2023-08-21 14:16:28 -0700

    Coreections

--- printf.c
+++ printf.c
@@ -11,39 +11,40 @@ int _printf(const char *format, ...)
 {
     int printed_chars = 0;
     va_list list;
+    int i = 0;
 
     if (format == NULL || (format[0] == '%' && format[1] == '\0'))
         return (-1);
 
     va_start(list, format);
 
-    for (int i = 0; *format != '\0'; format++)
+    for (i= 0; format[i] != '\0'; i++)
     {
-        if (*format != '%')
+        if (format[i] != '%')
         {
-            sam_charwriter(*format);
+            sam_charwriter(format[i]);
             printed_chars++;
         }
         else
         {
-            format++;
+            i++;
 
-            if (*format == 'c')
+            if (format[i] == 'c')
             {
                 char sam_ch = va_arg(list, int);
                 sam_charwriter(sam_ch);
                 printed_chars++;
             }
-            else if (*format == '%')
+            else if (format[i] == '%')
             {
                 sam_charwriter('%');
                 printed_chars++;
             }
-            else if (*format == 's')
+            else if (format[i] == 's')
             {
                 printed_chars += sam_str_print(va_arg(list, char *));
             }
-            else if (*format == 'd' || *format == 'i')
+            else if (format[i] == 'd' || format[i] == 'i')
             {
 int sam_num = va_arg(list, int);
                 printed_chars += Sam_negative(sam_num);

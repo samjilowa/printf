126c288289b08fe7561d3b094c8bc981f3b59263
KabzaKole <kabelomasela47@gmail.com>
2023-08-22 01:01:13 -0700

    Printf crrections

--- printf.c
+++ printf.c
@@ -9,50 +9,77 @@
 
 int _printf(const char *format, ...)
 {
-    int printed_chars = 0;
-    va_list list;
-    int i = 0;
+	va_list list;
+	int printed_chars = 0;
+	int i = 0;
 
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
 
-    va_start(list, format);
+	va_start(list, format);
 
-    for (i= 0; format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
-        {
-            sam_charwriter(format[i]);
-            printed_chars++;
-        }
-        else
-        {
-            i++;
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			sam_charwriter(format[i]);
+			printed_chars++;
+		}
+		else
+		{
+			i++;
+		}
 
-            if (format[i] == 'c')
-            {
-                char sam_ch = va_arg(list, int);
-                sam_charwriter(sam_ch);
-                printed_chars++;
-            }
-            else if (format[i] == '%')
-            {
-                sam_charwriter('%');
-                printed_chars++;
-            }
-            else if (format[i] == 's')
-            {
-                printed_chars += sam_str_print(va_arg(list, char *));
-            }
-            else if (format[i] == 'd' || format[i] == 'i')
-            {
-int sam_num = va_arg(list, int);
-                printed_chars += Sam_negative(sam_num);
-            }
-        }
-    }
+		if (format[i] == '\0')
+		{
+			va_end(list);
 
-    va_end(list);
+			return (-1);
+		}
 
-    return printed_chars;
+		if (format[i] == ' ')
+		{
+			va_end(list);
+
+			return (-1);
+		}
+
+		if (format[i] == 'c')
+		{
+			char sam_ch = va_arg(list, int);
+
+			sam_charwriter(sam_ch);
+			printed_chars++;
+		}
+		else if (format[i] == '%')
+		{
+			sam_charwriter('%');
+			printed_chars++;
+		}
+		else if (format[i] == 's')
+		{
+			char *str = va_arg(list, char *);
+		}
+		if (str == NULL)
+		{
+			sam_str_print("(null)");
+			printed_chars += 6;
+		}
+		else
+		{
+		printed_chars += sam_str_print(va_arg(list, char *));
+		}
+		}
+		else if (format[i] == 'd' || format[i] == 'i')
+		{
+			int sam_num = va_arg(list, int);
+
+			printed_chars += Sam_negative(sam_num);
+		}
+	}
+}
+
+va_end(list);
+
+return (printed_chars);
 }

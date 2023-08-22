9c256cfe1e2b1e8a9bce171560b8f8c5d7366bc6
samjilowa <samueljilowa@gmail.com.com>
2023-08-22 14:14:41 -0700

    printf executable

--- printf.c
+++ printf.c
@@ -1,39 +1,38 @@
 #include "main.h"
 #include <stddef.h>
-
 int _printf(const char *format, ...) {
-    va_list list;
-    int printed_chars = 0;
+va_list list;
+int printed_chars = 0;
 
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return -1;
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+return -1;
 
-    va_start(list, format);
+va_start(list, format);
 
-    for (; *format != '\0'; format++) {
-        if (*format != '%') {
-            sam_charwriter(*format);
-            printed_chars++;
-        } else {
-            format++;
+for (; *format != '\0'; format++) {
+if (*format != '%') {
+sam_charwriter(*format);
+printed_chars++;
+} else {
+format++;
 
-            if (*format == 'c') {
-                char sam_ch = va_arg(list, int);
-                sam_charwriter(sam_ch);
-                printed_chars++;
-            } else if (*format == '%') {
-                sam_charwriter('%');
-                printed_chars++;
-            } else if (*format == 's') {
-                printed_chars += sam_str_print(va_arg(list, char *));
-            } else if (*format == 'd' || *format == 'i') {
-                int sam_num = va_arg(list, int);
-                printed_chars += Sam_negative(sam_num);
-            }
-        }
-    }
+if (*format == 'c') {
+char sam_ch = va_arg(list, int);
+sam_charwriter(sam_ch);
+printed_chars++;
+} else if (*format == '%') {
+sam_charwriter('%');
+printed_chars++;
+} else if (*format == 's') {
+printed_chars += sam_str_print(va_arg(list, char *));
+} else if (*format == 'd' || *format == 'i') {
+int sam_num = va_arg(list, int);
+printed_chars += Sam_negative(sam_num);
+}
+}
+}
 
-    va_end(list);
+va_end(list);
 
-    return printed_chars;
+return printed_chars;
 }

bb102c87f3a1aa16952afc8507b8a18c8d78cb06
KabzaKole <kabelomasela47@gmail.com>
2023-08-22 18:14:56 -0700

    Resolved error messages

--- printf.c
+++ printf.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stddef.h>
 #include <stdarg.h>
-
 /**
  * _printf - Implementation of _printf function
  * @format: Format string containing the text and format specifier
@@ -10,49 +9,48 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list list;
-    int printed_chars = 0;
+	va_list list;
+	int printed_chars = 0;
 
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
+	va_start(list, format);
+	for (; *format != '\0'; format++)
+	{
+		if (*format != '%')
+		{
+		sam_charwriter(*format);
+		printed_chars++;
+		}
+		else
+		{
+		format++;
+		}
+		if (*format == 'c')
+		{
+			char sam_ch = va_arg(list, int);
 
-    va_start(list, format);
+			sam_charwriter(sam_ch);
+			printed_chars++;
+		}
+		else if (*format == '%')
+		{
+			sam_charwriter('%');
+			printed_chars++;
+		}
+		else if (*format == 's')
+		{
+			printed_chars += sam_str_print(va_arg(list, char *));
+		}
+		else if (*format == 'd' || *format == 'i')
+		{
+			int sam_num = va_arg(list, int);
 
-    for (; *format != '\0'; format++)
-    {
-        if (*format != '%')
-        {
-            sam_charwriter(*format);
-            printed_chars++;
-        }
-        else
-        {
-            format++;
+			printed_chars += Sam_negative(sam_num);
+		}
+	}
+}
 
-            if (*format == 'c')
-            {
-                char sam_ch = va_arg(list, int);
-                sam_charwriter(sam_ch);
-                printed_chars++;
-            }
-            else if (*format == '%')
-            {
-                sam_charwriter('%');
-                printed_chars++;
-            }
-            else if (*format == 's')
-            {
-                printed_chars += sam_str_print(va_arg(list, char *));
-            }
-            else if (*format == 'd' || *format == 'i')
-            {
-                int sam_num = va_arg(list, int);
-                printed_chars += Sam_negative(sam_num);
-            }
-        }
-    }
-
-    va_end(list);
-
-    return (printed_chars);
+	va_end(list);
+	return (printed_chars);
 }
--- sam_negative.c
+++ sam_negative.c
@@ -3,22 +3,21 @@
 /**
  * Sam_negative - Prints a negative number
  * @num: The integer to be printed
- * @count: Total count of characters printed
  *
  * Return: The number of characters printed
  */
 
 int Sam_negative(int num)
-{  
-int count = 0;
-
-if (num < 0)
 {
-count += sam_charwriter('-');
-num = -num;
-}
+	int count = 0;
 
-count += kabelo_print_i(num);
+	if (num < 0)
+	{
+		count += sam_charwriter('-');
+		num = -num;
+	}
 
-return (count);
+	count += kabelo_print_i(num);
+
+	return (count);
 }

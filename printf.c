0179c87621f6aa99d7d3a089cdf3723151b3f35e
samjilowa <samueljilowa@gmail.com.com>
2023-08-22 18:23:34 -0700

    printf

--- printf.c
+++ printf.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stddef.h>
 #include <stdarg.h>
+
 /**
  * _printf - Implementation of _printf function
  * @format: Format string containing the text and format specifier
@@ -9,48 +10,49 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list list;
-	int printed_chars = 0;
+    va_list list;
+    int printed_chars = 0;
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-	return (-1);
-	va_start(list, format);
-	for (; *format != '\0'; format++)
-	{
-		if (*format != '%')
-		{
-		sam_charwriter(*format);
-		printed_chars++;
-		}
-		else
-		{
-		format++;
-		}
-		if (*format == 'c')
-		{
-			char sam_ch = va_arg(list, int);
+    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+        return (-1);
 
-			sam_charwriter(sam_ch);
-			printed_chars++;
-		}
-		else if (*format == '%')
-		{
-			sam_charwriter('%');
-			printed_chars++;
-		}
-		else if (*format == 's')
-		{
-			printed_chars += sam_str_print(va_arg(list, char *));
-		}
-		else if (*format == 'd' || *format == 'i')
-		{
-			int sam_num = va_arg(list, int);
+    va_start(list, format);
 
-			printed_chars += Sam_negative(sam_num);
-		}
-	}
-}
+    for (; *format != '\0'; format++)
+    {
+        if (*format != '%')
+        {
+            sam_charwriter(*format);
+            printed_chars++;
+        }
+        else
+        {
+            format++;
 
-	va_end(list);
-	return (printed_chars);
+            if (*format == 'c')
+            {
+                char sam_ch = va_arg(list, int);
+                sam_charwriter(sam_ch);
+                printed_chars++;
+            }
+            else if (*format == '%')
+            {
+                sam_charwriter('%');
+                printed_chars++;
+            }
+            else if (*format == 's')
+            {
+                printed_chars += sam_str_print(va_arg(list, char *));
+            }
+            else if (*format == 'd' || *format == 'i')
+            {
+                int sam_num = va_arg(list, int);
+                printed_chars += Sam_negative(sam_num);
+            }
+        }
+    }
+
+    va_end(list);
+
+    return (printed_chars);
 }

4ca5f27b3f5386efecdde36366ca879de9f0a1f8
KabzaKole <kabelomasela47@gmail.com>
2023-08-22 17:21:20 -0700

    Resolved conflict

--- printf.c
+++ printf.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 #include <stddef.h>
 #include <stdarg.h>
 
@@ -12,49 +12,52 @@
 
 int _printf(const char *format, ...)
 {
-    va_list list;
-    int printed_chars = 0;
+	va_list list;
+	int printed_chars = 0;
 
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
 
-    va_start(list, format);
+	va_start(list, format);
 
-    for (; *format != '\0'; format++)
-    {
-        if (*format != '%')
-        {
-            sam_charwriter(*format);
-            printed_chars++;
-        }
-        else
-        {
-            format++;
+	for (; *format != '\0'; format++)
+	{
+		if (*format != '%')
+		{
+			sam_charwriter(*format);
+			printed_chars++;
+		}
+		else
+		{
+			format++;
+		}
 
-            if (*format == 'c')
-            {
-                char sam_ch = va_arg(list, int);
-                sam_charwriter(sam_ch);
-                printed_chars++;
-            }
-            else if (*format == '%')
-            {
-                sam_charwriter('%');
-                printed_chars++;
-            }
-            else if (*format == 's')
-            {
-                printed_chars += sam_str_print(va_arg(list, char *));
-            }
-            else if (*format == 'd' || *format == 'i')
-            {
-                int sam_num = va_arg(list, int);
-                printed_chars += Sam_negative(sam_num);
-            }
-        }
-    }
+		if (*format == 'c')
+		{
+			char sam_ch = va_arg(list, int);
 
-    va_end(list);
+			sam_charwriter(sam_ch);
+			printed_chars++;
+		}
+		else if (*format == '%')
+		{
+			sam_charwriter('%');
+			printed_chars++;
+		}
+		else if (*format == 's')
+		{
+			printed_chars += sam_str_print(va_arg(list, char *));
+		}
+		else if (*format == 'd' || *format == 'i')
+		{
+			int sam_num = va_arg(list, int);
 
-    return (printed_chars);
+			printed_chars += Sam_negative(sam_num);
+		}
+	}
+}
+
+	va_end(list);
+
+	return (printed_chars);
 }
